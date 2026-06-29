e9bfe5518b76baf7b6e74d1dc22e1eb8cfbb2e78
Nahyun Lee <nh980223@gmail.com>
2026-06-29 16:14:48 -0700

    Final Pipeline

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+PYTHONWARNINGS="ignore::DeprecationWarning" jupyter nbconvert \
+  --to notebook \
+  --execute notebooks/final_pipeline.ipynb \
+  --output final_pipeline_executed.ipynb \
+  --output-dir /tmp
